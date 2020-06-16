4c4b16ab94c560ff02d121f4ff10c765b27f9c82
brooder_code <c_schiebelbein@hotmail.com>
2020-06-16 16:38:30 -0600

    Bug fix

--- components/Layout.js
+++ components/Layout.js
@@ -6,8 +6,14 @@ import Head from 'next/head';
 const Layout = (props) => (
     <div>
         <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169748631-1"></script>
-
+        <script dangerouslySetInnerHTML={{
+    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-TFF64VX'');`,
+  }}>
+  </script>
             <title>The Opinion Bowl</title>
             <link rel="stylesheet"
             href="https://bootswatch.com/4/flatly/bootstrap.css" />

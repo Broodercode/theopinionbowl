56f683875b9fe893d33b50dfde2b34cf41a9a75b
brooder_code <c_schiebelbein@hotmail.com>
2020-06-16 16:18:34 -0600

    Bug fix

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
 import NavBar from './NavBar';
 import Head from 'next/head';
 
+
+
 const Layout = (props) => (
     <div>
         <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169748631-1"></script>
+
             <title>The Opinion Bowl</title>
             <link rel="stylesheet"
             href="https://bootswatch.com/4/flatly/bootstrap.css" />
@@ -13,14 +17,7 @@ const Layout = (props) => (
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <script data-ad-client="ca-pub-6241376391251116" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> 
      
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-169748631-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
 
-  gtag('config', 'UA-169748631-1');
-</script>
         </Head>
         <NavBar />
         <div className="container">

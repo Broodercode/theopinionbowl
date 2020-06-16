8143f26e01d275b389ee75cd1e433da4db6a047b
brooder_code <c_schiebelbein@hotmail.com>
2020-06-16 12:11:40 -0600

    meta tags

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,11 @@ const Layout = (props) => (
             <title>The Opinion Bowl</title>
             <link rel="stylesheet"
             href="https://bootswatch.com/4/flatly/bootstrap.css" />
-            <script data-ad-client="ca-pub-6241376391251116" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <meta name="description" content="A website dedicated to sharing the entire breadth of a given controversy" />
+            <meta name="keywords" content='free speech freedom of expression freedom of speech definition what is free speech' />
+            <meta name="robots" content="all" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <script data-ad-client="ca-pub-6241376391251116" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> 
         </Head>
         <NavBar />
         <div className="container">

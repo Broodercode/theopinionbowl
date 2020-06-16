5226193624d802112b02fb3ba196dcba87f822a2
brooder_code <c_schiebelbein@hotmail.com>
2020-06-16 10:34:43 -0600

    google adsense link

--- components/Layout.js
+++ components/Layout.js
@@ -7,6 +7,7 @@ const Layout = (props) => (
             <title>The Opinion Bowl</title>
             <link rel="stylesheet"
             href="https://bootswatch.com/4/flatly/bootstrap.css" />
+            <script data-ad-client="ca-pub-6241376391251116" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         </Head>
         <NavBar />
         <div className="container">

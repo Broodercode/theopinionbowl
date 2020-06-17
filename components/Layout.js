e1fdc6afad96d08fd50c98f302312dd8fc2e9a75
brooder_code <c_schiebelbein@hotmail.com>
2020-06-16 18:45:15 -0600

    Google analytics

--- components/Layout.js
+++ components/Layout.js
@@ -6,13 +6,19 @@ import Head from 'next/head';
 const Layout = (props) => (
     <div>
         <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169748631-1"></script>
         <script dangerouslySetInnerHTML={{
-    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-TFF64VX'');`,
+    __html: `window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'UA-169748631-1');`,
   }}>
+
+
+
+
+
   </script>
             <title>The Opinion Bowl</title>
             <link rel="stylesheet"
